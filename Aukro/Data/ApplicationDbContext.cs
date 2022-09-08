c9262e790918eba70d2b0910a8e8145bceccb541
Vít Zeman <vitzema019@pslib.cz>
2022-09-08 11:27:35 +0200

    Přidejte soubory projektu.

---
+++ Aukro/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aukro.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book>? Books { get; set; }
+        public ApplicationDbContext() : base()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite(@"Data Source=myBooks.sqlite");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Book>()
+                .HasIndex(u => u.BookId)
+                .IsUnique();
+            builder.Entity<Book>().HasData(new Book { BookId = 1, Title = "Společenstvo prstenu", Pages = 300 });
+        }
+    }
+}
