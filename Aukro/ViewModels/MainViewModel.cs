0e0cf3d83d230f8d59a9d3c2fd06b459c3455abe
Vít Zeman <vit.zem@seznam.cz>
2022-09-23 06:58:17 +0200

    login nevim

--- Aukro/ViewModels/MainViewModel.cs
+++ Aukro/ViewModels/MainViewModel.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Aukro.ViewModels
 {
@@ -18,6 +19,7 @@ namespace Aukro.ViewModels
         private ObservableCollection<Auction> _auctions;
         private string _currentUser;
         private int? _UserId;
+        private string? _LoginErrorMessage;
 
         public RelayCommand ShowCommand { get; set; }
         public ParametrizedRelayCommand<User> LoginCommand { get; set; }
@@ -47,8 +49,22 @@ namespace Aukro.ViewModels
                 async (user) => {
                     if (Db != null)
                     {
-                        User u = Db.Users.Where(x=>x.Username == user.Username && x.Password == user.Password).SingleOrDefault();
-                        CurrentUser = u.Username;
+
+                        User u = Db.Users.Where(x => x.Username == user.Username && x.Password == user.Password).SingleOrDefault();
+                        if(u != null) 
+                        {
+                            CurrentUser = u.Username;
+                        }
+                        else 
+                        {
+                            MessageBox.Show("Unable to Login, incorrect credentials.");
+                        }
+                            
+
+
+                            
+
+                        
                     }
                 }
                 );
@@ -56,7 +72,12 @@ namespace Aukro.ViewModels
 
         
 
-public string CurrentUser
+        public string LoginErrorMessage 
+        {
+            get { return _LoginErrorMessage; }
+            set { _LoginErrorMessage = value; NotifyPropertyChanged(); }
+        }
+        public string CurrentUser
         {
             get { return _currentUser; }
             set { _currentUser = value; NotifyPropertyChanged(); }
